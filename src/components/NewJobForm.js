7e7f8f37fe3c964afe230ed1a51295bfb3d2b2cd
heartandkarma <heartandkarma@gmail.com>
2022-04-20 15:01:47 -0700

    got form visual output working... gotta setup form state now and get it under control....

--- src/components/NewJobForm.js
+++ src/components/NewJobForm.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 function NewJobForm() {
-  return <div>form to add new jobs</div>;
+  const submitHandle = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <h1>New Job Form</h1>
+      <form onSubmit={submitHandle}>
+        <label>
+          Job Title:
+          <input type="text" name="jobTitle" />
+        </label>
+        <label>
+          Job Link:
+          <input type="text" name="jobLink" />
+        </label>
+        <label>
+          Researched:
+          <input type="checkbox" name="researched" />
+        </label>
+        <label>
+          Notes:
+          <input type="text" name="notes" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default NewJobForm;
